940c7a2bb473331bad0cc402abbb0d4f2e901ffc
Ícaro Teodoro <icaro.teodoro84@icloud.com>
2025-04-09 21:25:22 -0300

    feat: create functions for get items in list format of items and get order in order response format

--- src/main/java/com/entrego/domain/Order.java
+++ src/main/java/com/entrego/domain/Order.java
@@ -3,13 +3,15 @@ package com.entrego.domain;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
+import com.entrego.dtos.ItemsOrderResponse;
 import com.entrego.dtos.OrderDTO;
 
+import com.entrego.dtos.OrderResponse;
 import com.entrego.enums.OrderStatus;
 import com.entrego.enums.PaymentMethod;
+import com.entrego.repositories.AddressRepository;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -60,6 +62,43 @@ public class Order {
 		Random random = new Random();
 		int randomNumber = random.nextInt(9000) + 1000;
 		return String.valueOf(randomNumber);
+	}
+
+	public List<ItemsOrderResponse> getListItems() {
+		List<Product> products = this.getProducts();
+		List<ItemsOrderResponse> items = new ArrayList<>();
+
+		Map<Product, Integer> productCount = new HashMap<>();
+
+		for (Product product : products) {
+			productCount.put(product, productCount.getOrDefault(product, 0) + 1);
+		}
+
+		for (Map.Entry<Product, Integer> entry : productCount.entrySet()) {
+			Product product = entry.getKey();
+			int quantity = entry.getValue();
+			double totalPrice = product.getPrice() * quantity;
+
+			items.add(new ItemsOrderResponse(
+					product.getName(),
+					totalPrice,
+					quantity
+			));
+		}
+		return items;
+	}
+
+	public OrderResponse getOrderResponse(AddressRepository addressRepository) {
+        return new OrderResponse(
+				this.getId(),
+				this.getUser().getFirstName() + " " + this.getUser().getLastName(),
+				this.getNumberOrder(),
+				this.getCreatedAt(),
+				this.getStatus(),
+				addressRepository.findAddressByUserIdAndIsMain(this.getUser().getId()),
+				this.getPaymentMethod(),
+				this.getListItems()
+		);
 	}
 
 }
