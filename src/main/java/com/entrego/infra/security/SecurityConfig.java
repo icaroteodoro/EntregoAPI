a5ed6e63eae6f11d10e76013dfe339f62ccac8a8
Ícaro Teodoro <icaro.teodoro84@icloud.com>
2025-04-09 21:29:51 -0300

    feat: set new config to release websocket endpoints

--- src/main/java/com/entrego/infra/security/SecurityConfig.java
+++ src/main/java/com/entrego/infra/security/SecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -33,6 +38,9 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/store/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/store/refresh-token").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/user/refresh-token").permitAll()
+                        .requestMatchers( "/ws/**").permitAll()
+                        .requestMatchers( "/topic/**").permitAll()
+                        .requestMatchers( "/app/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
@@ -48,4 +56,17 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+    //    @Bean
+    //    public CorsConfigurationSource corsConfigurationSource() {
+    //        CorsConfiguration configuration = new CorsConfiguration();
+    //        configuration.setAllowedOrigins(Arrays.asList("*"));
+    //        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    //        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+    //        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+    //        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    //        source.registerCorsConfiguration("/**", configuration);
+    //        return source;
+    //    }
+    // talvez não precise por que já tem um configuração de cors em./CorsFilter e ./CorsFilter
+
 }
