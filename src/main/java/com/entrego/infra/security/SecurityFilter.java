fab429460ca6cd5a39570920c638f36a2f5b16d0
Ícaro Teodoro <icaro.teodoro84@icloud.com>
2025-04-09 21:31:23 -0300

    feat: set an endpoint that should not be filtered for websocket

--- src/main/java/com/entrego/infra/security/SecurityFilter.java
+++ src/main/java/com/entrego/infra/security/SecurityFilter.java
@@ -30,6 +30,12 @@ public class SecurityFilter extends OncePerRequestFilter {
 
 
     @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getRequestURI();
+        return path.startsWith("/ws");
+    }
+
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
         var login = tokenService.validateToken(token);
